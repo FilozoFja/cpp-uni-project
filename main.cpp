ab7fcb5fcd946a16f782d0fa3d836513f5dfd0d1
Kornel Pustelak <92749743+FilozoFja@users.noreply.github.com>
2025-12-11 15:28:14 +0100

    immunity-class
    (cherry picked from commit e5fdb86a0ae8fa86395c6276c45552c3ba98b505)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,5 @@
 #include <iostream>
 
 int main() {
-    for (int i = 0; i < 1000000; ++i) {
-    std::cout << "ZARAZ NA KLUCHY KUUUUUUUUUUURWAAAAAAAAAAAAAAAAAAAAAAAAAA" << std::endl;
-
-    }
     return 0;
 }
