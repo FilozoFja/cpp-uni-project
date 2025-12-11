b36298b7a323c461352235751130c7e8e910d9b1
Kornel Pustelak <92749743+FilozoFja@users.noreply.github.com>
2025-12-11 09:14:43 +0100

    wip
    (cherry picked from commit de9f41cc62db40a1e86dbda606ac6b429523f39f)

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
 int main() {
+    for (int i = 0; i < 1000000; ++i) {
+    std::cout << "ZARAZ NA KLUCHY KUUUUUUUUUUURWAAAAAAAAAAAAAAAAAAAAAAAAAA" << std::endl;
+
+    }
     return 0;
 }
